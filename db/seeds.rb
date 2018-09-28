f33297f8464aecb246b796c27001388697ef491c
heqifatiha <heqifatihaputri@gmail.com>
2018-09-28 14:19:55 +0700

    modified db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'heqifatiha9@gmail.com', password: 'admin12345', password_confirmation: 'admin12345') if Rails.env.development?
