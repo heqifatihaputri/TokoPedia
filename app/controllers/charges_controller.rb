d8770cba76ee1c0b4bef62f3a08c94687b162f9c
heqifatiha <heqifatihaputri@gmail.com>
2018-09-28 13:50:18 +0700

    change chargers

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,7 +16,7 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       :customer     => customer.id,
       :amount       => @amount,
-      :description  => 'Rails Stripe customer'
+      :description  => 'Rails Stripe customer',
       :currency     => 'usd'
     )
 
