1a24154c05df5a6ce3a295bf2ccbc46e4caed5e3
heqifatiha <heqifatihaputri@gmail.com>
2018-09-28 13:31:46 +0700

    remove order as fk

--- db/migrate/20180910012427_create_carts.rb
+++ db/migrate/20180910012427_create_carts.rb
@@ -2,7 +2,6 @@ class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
       t.references :product, foreign_key: true
-      t.references :order, foreign_key: true
 
       t.timestamps
     end
