7649ace711b498c2756495ae00aea0e64fb71168
heqifatiha <heqifatihaputri@gmail.com>
2018-10-02 09:28:53 +0700

    revisi stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['pk_test_LKDzCsEwekIqKLear1qql8qo'],
+  :secret_key      => ENV['sk_test_QxRSGkjGBDLQXdT4bd9YPSiR']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
